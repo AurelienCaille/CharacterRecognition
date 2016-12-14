ae869ba5c6077aa68a1a935459c02598e606b1a5
Lievre Antoine <alievre@laposte.net>
2016-12-14 08:59:14 +0100

    refresh

--- scikilearn_method.py
+++ scikilearn_method.py
@@ -1,32 +1,40 @@
-print(__doc__)
-from openMNIST import load_mnist
-
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
-
+from openMNIST import load_mnist
 # Import datasets, classifiers and performance metrics
-from sklearn import datasets, svm, metrics
+from sklearn import svm, metrics
 
 # The data set from MMNIST
 images, labels = load_mnist()
-
-
+images = images[:1000]
+labels = labels[:1000]
+indexs = range(1000)
 # To apply a classifier on this data, we need to flatten the image, to
 # turn the data in a (samples, feature) matrix:
+
 n_samples = len(images)
+
+
+for index, (image, label) in indexs, (images, labels):
+    plt.subplot(2, 4, index + 1)
+    plt.axis('off')
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title('Training: %i' % label)
 data = images.reshape((n_samples, -1))
+print(data[3])
 
 # Create a classifier: a support vector classifier
-classifier = svm.SVC(gamma=0.001)
+classifier = svm.SVC()
 
 # We learn the digits on the first half of the digits
 classifier.fit(data[:n_samples / 2], labels[:n_samples / 2])
 
 # Now predict the value of the digit on the second half:
 expected = labels[n_samples / 2:]
+
 predicted = classifier.predict(data[n_samples / 2:])
 
 print("Classification report for classifier %s:\n%s\n"
